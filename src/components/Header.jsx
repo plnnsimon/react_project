1bb727bf0645639a31b4aae4a051486a0668fdb9
Олександр Пальонний <s2567k@gmail.com>
2021-04-02 13:42:38 +0300

    react_project

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header className={classes.header}>
+            <img src="https://images.coollogo.com/images/prism-large-green.png" />
+        </header>
+    )
+
+}   
+
+
+export default Header
